d6bd6e580ff8d9da48ce0cf45c78a643e9bcd1a6
WeslenAlmeida <weslen1202@gmail.com>
2022-10-22 09:58:44 -0300

    Add project files.

---
+++ ApiCep/Controllers/AddressController.cs
@@ -0,0 +1,27 @@
+﻿using ApiCep.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCep.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressController : ControllerBase
+    {
+        public AddressController()
+        {
+             //Como no método GetAddress já temos a requisição HTTP, podemos deixar o método construtor vazio
+        }
+
+        [HttpGet("{cep}")]
+        public ActionResult<string> Get(string cep)
+        {
+            var address = new AddressService().GetAddress(cep); 
+
+            if (address == null)
+                return NotFound();
+
+            return Ok(address.Result);
+        }
+    }
+}
---
+++ ApiCep/Services/AddressService.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Net;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ApiCep.Service
+{
+    public class AddressService
+    {
+       
+        
+        public AddressService()
+        {
+         
+        }
+
+        //Método para executar uma requisição web
+        public async Task<string> GetAddress(string cep)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://viacep.com.br/ws/" + cep + "/json/");
+            request.AllowAutoRedirect = false;
+            HttpWebResponse ChecaServidor = (HttpWebResponse)request.GetResponse();
+
+            Stream web = ChecaServidor.GetResponseStream();
+
+            if (web == null)
+                return null;
+
+            StreamReader responseReader = new StreamReader(web);
+
+            //Caso a requisição obtenha uma resposta ela insere na string responseText
+            string responseText = responseReader.ReadToEnd();
+            
+            return responseText;
+        }
+             
+    }
+}
